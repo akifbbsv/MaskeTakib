6bb2682c2653211f8476e1d839ae3fd3b4da9e77
akifbbsv <Akif_90@mail.ru>
2024-02-06 15:24:01 +0400

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+    internal class Program
+    {
+
+
+    static void Main(string[] args)
+    {
+            Person person1 = new Person();
+            person1.FirstName = "Akif";
+            person1.LastName = "Abbasov";
+            person1.DateOfBirthYear = 1997;
+            person1.NationalIdentity = 123;
+
+
+            Person person2 = new Person();
+            person2.FirstName = "Murat";
+    
+
+        PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+    }
+
+}
+    
+}
